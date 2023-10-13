1618f6d2b10212bef73a3b0737ac84f100e48d72
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-10-13 15:44:33 +0200

    Changed order of "if" checks

--- eme3/eme3.cs
+++ eme3/eme3.cs
@@ -9,17 +9,17 @@ int last = roz % 10;
 int pol = 0;
 //Console.Write($"Witaj {input[0]}! ");
 
-if (roz == 1)
+if (wiek < 0 || emeryt < 0)
 {
-    pol = 1;
+    pol = 4;
 }
 else if (roz == 0)
 {
     pol = 5;
 }
-else if (wiek < 0 || emeryt < 0)
+else if (roz == 1)
 {
-    pol = 4;
+    pol = 1;
 }
 else if (last > 1 && last < 5)
 {
